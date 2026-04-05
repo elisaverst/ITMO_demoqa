56486e8581528306132e47dbc2b1141d6de96164
elisaverst <lisa.grustnaya@gmail.com>
2026-04-05 14:33:15 +0300

    hw #7 upd

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -2,18 +2,23 @@ from pages.demoqa import DemoQA
 from pages.elements_page import ElementsPage
 import time
 
-def test_check_icon(browser):
+def test_check_footer(browser):
 
     demo_qa_pages = DemoQA(browser)
-    elements_page = ElementsPage(browser)
 
     demo_qa_pages.visit()
     time.sleep(3)
 
     assert demo_qa_pages.footer.get_text() == '© 2013-2026 TOOLSQA.COM | ALL RIGHTS RESERVED.'
 
+def test_check_text(browser):
+    demo_qa_pages = DemoQA(browser)
+    elements_page = ElementsPage(browser)
+
+    demo_qa_pages.visit()
     demo_qa_pages.button_elements.click()
     time.sleep(3)
 
     assert elements_page.equal_url()
     assert elements_page.text.get_text() == 'Please select an item from left to start practice.'
+
